3008fae528cfcfdef4b58138a16e0d012f78b4ee
mlholland <mlh244@gmail.com>
2014-03-06 20:50:31 -0500

    Small comments on launchState
    Mostly notes from spec discussion with Gaurav.

--- production/Cutlass/src/edu/brown/cs/cutlass/LaunchState.java
+++ production/Cutlass/src/edu/brown/cs/cutlass/LaunchState.java
@@ -6,16 +6,21 @@
 package edu.brown.cs.cutlass;
 
 import edu.brown.cs.cutlass.sys.io.AbstractIdentifier;
+
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Holds (and handles serializing/unserializing of) the program state at launch.
  * This includes the files currently open.
  *
  * @author Gaurav Manek
+ * T is an arrPointer
  */
 class LaunchState<T extends AbstractIdentifier> {
     
+	// T is an AbstractIdentifer that identifies a unique resource on the 
+	// current I/O media (Ex. a path object to a space in disk).
     private final ArrayList<T> openFiles;
 
     private LaunchState(ArrayList<T> openFiles) {
@@ -27,8 +32,21 @@ class LaunchState<T extends AbstractIdentifier> {
         throw new UnsupportedOperationException("Not yet written");
     }
     
+    /**Work in progress: Specs of fromString input string:
+     * Must include:
+     * file path representation
+     * anything we decide on later.
+     */
     
-    public static LaunchState fromString(String in) throws IllegalArgumentException {
+    
+    /** 
+     * 
+     * @param in A string representation of the launch state. Includes:
+     * 
+     * @return A LaunchState object
+     * @throws IllegalArgumentException
+     */
+    public static LaunchState fromString(List<String> in) throws IllegalArgumentException {
         throw new UnsupportedOperationException("Not yet written");
     }
     
