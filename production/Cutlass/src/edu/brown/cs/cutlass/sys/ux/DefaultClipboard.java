f5f3aa7434a921aff70554a499dee5787907174a
zzagorsk <zzagorsk@cs.brown.edu>
2014-04-14 15:20:17 -0400

    Clipboard first draft?

--- production/Cutlass/src/edu/brown/cs/cutlass/sys/ux/DefaultClipboard.java
+++ production/Cutlass/src/edu/brown/cs/cutlass/sys/ux/DefaultClipboard.java
@@ -5,20 +5,53 @@
 
 package edu.brown.cs.cutlass.sys.ux;
 
+import java.awt.Toolkit;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.ClipboardOwner;
+import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.StringSelection;
+import java.awt.datatransfer.Transferable;
+import java.awt.datatransfer.UnsupportedFlavorException;
+import java.io.IOException;
+
 /**
  *
- * @author Gaurav Manek
+ * @author Gaurav Manek, Zachary Zagorski
  */
 public class DefaultClipboard implements AbstractClipboard {
 
     @Override
     public void put(CharSequence s) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        StringSelection stringSelection = new StringSelection(s.toString());
+        Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+        clipboard.setContents(stringSelection, this);
     }
 
     @Override
     public String get() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        String result = "";
+        Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+        //odd: the Object param of getContents is not currently used
+        Transferable contents = clipboard.getContents(null);
+        boolean hasTransferableText =
+          (contents != null) &&
+          contents.isDataFlavorSupported(DataFlavor.stringFlavor)
+        ;
+        if (hasTransferableText) {
+          try {
+            result = (String)contents.getTransferData(DataFlavor.stringFlavor);
+          }
+          catch (UnsupportedFlavorException | IOException ex){
+            System.out.println(ex);
+            ex.printStackTrace();
+          }
+        }
+        return result;    
+    }
+
+    @Override
+    public void lostOwnership(Clipboard clipboard, Transferable contents) {
+        //do nothing
     }
     
 }
